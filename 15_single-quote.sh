ebddcf6163bd784050feccfdd4cf463e73f9cc86
astinchoi <asciineo@gmail.com>
2016-07-11 09:42:27 +0900

    single quote

--- 15_single-quote.sh
+++ 15_single-quote.sh
@@ -5,9 +5,13 @@
 price=100
 str='It costs $'$price'? I can'\''t believe it!'
 echo $str
+# It costs $100? I can't believe it!
 
 echo "My Terminal: $TERM"
 echo 'My Terminal: $TERM'
+# My Terminal: xterm
+# My Terminal: $TERM
 
 str='He said "Hello!". I said "Hello".'
 echo $str
+# He said "Hello!". I said "Hello".
