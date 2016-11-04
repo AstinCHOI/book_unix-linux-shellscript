1930a9115e21b92cc41e341bbf19024cbbe0ca5f
astinchoi <astiineo@gmail.com>
2016-11-04 15:52:59 -0500

    extract info from data

---
+++ 6_text/72_csv-select.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Use: cut, read, echo
+
+csvfile="data.csv"
+
+if [ -z "$1" ]; then
+  echo "There is no ID." >&2
+  exit 1
+fi
+
+if [ ! -f "$csvfile" ]; then
+  echo "CSV file doesn't exist: $csvfile" >&2
+  exit 1
+fi
+
+while read line
+do
+  # Extract colmuns 
+  id=$(echo $line | cut -f 1 -d ',')
+  name=$(echo $line | cut -f 2 -d ',')
+  score=$(echo $line | cut -f 3 -d ',')
+
+  
+  # If ID match with argument
+  if [ "$1" = "$id" ]; then
+    echo "$name"
+  fi
+done < $csvfile
+
